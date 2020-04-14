18ff5759799fa07d3a018a09697e27d207dc8331
rond <ron.dahan@campus.technion.ac.il>
2020-04-14 12:55:50 +0300

    ADT

--- map.c
+++ map.c
@@ -1,11 +1,19 @@
 //
 // Created by Ron Dahan on 14/04/2020.
 //
-
+#include <stdlib.h>
 #include "map.h"
+
+struct Map_t{
+    char** key;
+    char** value;
+};
+
 void mapDestroy(Map map) {
+
 }
 
 Map mapCreate(){
+    Map map = malloc(sizeof(*map));
     return NULL;
 }
